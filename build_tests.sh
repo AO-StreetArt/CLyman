aa39d228ee7a52209d121744894a7637c5ef2e76
ao-streetart <aostreetart9@gmail.com>
2016-05-13 22:03:51 -0400

    Build Tests Script modified

--- build_tests.sh
+++ build_tests.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-g++ -c -o obj3_test.o obj3_test.cpp
+g++ -c -o src/tests/obj3_test.o src/tests/obj3_test.cpp -std=c++11
 
 #Here we can build the test app:
 
-g++ -lpthread -llog4cpp -o obj_test ../logging.o ../obj3.o obj3_test.o
+g++ -lpthread -llog4cpp -o obj_test src/logging.o src/obj3.o src/tests/obj3_test.o -std=c++11
 
 #Now, we get to build the main test app:
 
-g++ -c -llog4cpp -lpthread -lzmq -I /usr/local/lib -o main_class_test.o main_class_test.cpp
+g++ -c -llog4cpp -lpthread -lzmq -I /usr/local/lib -o src/tests/main_class_test.o src/tests/main_class_test.cpp -std=c+11
 
-g++ -o main_test ../event_dispatcher.o main_class_test.o -llog4cpp -lpthread -lzmq
+g++ -o main_test src/event_dispatcher.o src/tests/main_class_test.o -llog4cpp -lpthread -lzmq -std=c++11
 
 #And the test client:
 
-g++ -I /usr/local/lib test_client.cpp -o test_client -lzmq
+#g++ -I /usr/local/lib test_client.cpp -o src/tests/test_client -lzmq
 
 #We can build the couchbase tests:
 
-g++ -c -o couchbase_test.o -lcouchbase couchbase_test.cpp -std=c++11
+g++ -c -o src/tests/couchbase_test.o -lcouchbase src/tests/couchbase_test.cpp -std=c++11
 
 #And the couchbase test app:
 
-g++ -o cb_test event_dispatcher.o ../couchbase_admin.o couchbase_test.o ../obj3.o -lcouchbase -std=c++11
+g++ -o cb_test src/event_dispatcher.o src/couchbase_admin.o src/tests/couchbase_test.o src/obj3.o -lcouchbase -std=c++11
