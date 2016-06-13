5864f9362020983d4b3f5ce49c008c843e687bc9
ao-streetart <aostreetart9@gmail.com>
2016-06-12 23:01:26 -0400

    Redis Tests Pushed

--- build_tests.sh
+++ build_tests.sh
@@ -23,3 +23,8 @@ g++ -c -o src/tests/couchbase_test.o -lcouchbase src/tests/couchbase_test.cpp -s
 #And the couchbase test app:
 
 g++ -o cb_test src/event_dispatcher.o src/couchbase_admin.o src/tests/couchbase_test.o src/obj3.o -lcouchbase -std=c++11
+
+#Finally, the Redis test app:
+g++ -c -o src/tests/redis_test.o src/tests/redis_test.cpp -lpthread -llog4cpp -lxredis `pkg-config --cflags --libs hiredis` -I src -std=c++11 
+
+g++ -o src/tests/redis_test -lpthread -llog4cpp -lxredis src/logging.o src/xredis_admin.o src/tests/redis_test.o `pkg-config --cflags --libs hiredis` -I src -std=c++11
