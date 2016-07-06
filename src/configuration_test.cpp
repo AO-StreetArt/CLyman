7847c8aa0b5b3c123f3f3633d10d66b1ecee605f
ao-streetart <aostreetart9@gmail.com>
2016-07-06 02:06:41 -0400

    Fixes after refactor

--- src/configuration_test.cpp
+++ src/configuration_test.cpp
@@ -23,25 +23,24 @@ int main()
   assert ( cm.get_subduration() == 240 );
 
   //Redis Connection List Tests
-  std::vector<RedisConnChain> RedisConnectionList = cm->get_redisconnlist();
+  std::vector<RedisConnChain> RedisConnectionList = cm.get_redisconnlist();
   int conn_list_size = RedisConnectionList.size();
-  RedisNode RedisList1[conn_list_size];
 
   RedisConnChain redis_chain = RedisConnectionList[0];
   assert( redis_chain.ip == "127.0.0.1" );
-  assert( redis_chain.port == "6379" );
+  assert( redis_chain.port == 6379 );
   assert( redis_chain.elt4 == "test" );
-  assert( redis_chain.elt5 == "2");
-  assert( redis_chain.elt6 == "5");
-  assert( redis_chain.elt7 == "0");
+  assert( redis_chain.elt5 == 2);
+  assert( redis_chain.elt6 == 5);
+  assert( redis_chain.elt7 == 0);
 
   RedisConnChain redis_chain2 = RedisConnectionList[1];
   assert( redis_chain2.ip == "127.0.0.1" );
-  assert( redis_chain2.port == "6380" );
+  assert( redis_chain2.port == 6380 );
   assert( redis_chain2.elt4 == "test2" );
-  assert( redis_chain2.elt5 == "2");
-  assert( redis_chain2.elt6 == "5");
-  assert( redis_chain2.elt7 == "0");
+  assert( redis_chain2.elt5 == 2);
+  assert( redis_chain2.elt6 == 5);
+  assert( redis_chain2.elt7 == 0);
 
 
   return 0;
