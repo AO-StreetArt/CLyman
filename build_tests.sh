786528c336f1a3e2d8d7f325a52f3e35dbaf6b37
ao-streetart <aostreetart9@gmail.com>
2016-07-06 01:10:23 -0400

    Major Refactor & Tests Revamped

--- build_tests.sh
+++ build_tests.sh
@@ -7,7 +7,7 @@ g++ -o configuration_test src/logging.o src/configuration_manager.o src/configur
 
 #Here we have the obj3 tests
 
-g++ -c -o src/obj3_test.o -lpthread -llog4cpp src/obj3_test.cpp `pkg-config --cflags --libs protobuf` -std=c++11
+g++ -c -o src/obj3_test.o src/obj3_test.cpp -std=c++11
 g++ -o obj_test src/Obj3.pb.cc src/logging.o src/obj3.o src/obj3_test.o -lpthread -llog4cpp `pkg-config --cflags --libs protobuf` -std=c++11
 
 #And the object benchmark
@@ -17,7 +17,7 @@ g++ -o obj3_benchmark src/logging.o src/Obj3.pb.cc src/obj3.o src/obj3_benchmark
 
 #We can build the couchbase tests:
 
-g++ -c -o src/couchbase_test.o -lcouchbase src/couchbase_test.cpp -I src -std=c++11
+g++ -c -o src/couchbase_test.o src/couchbase_test.cpp -std=c++11
 g++ -o cb_test src/Obj3.pb.cc src/logging.o src/event_dispatcher.o src/couchbase_admin.o src/couchbase_test.o src/obj3.o -lpthread -llog4cpp -lcouchbase `pkg-config --cflags --libs protobuf` -std=c++11
 
 #And the couchbase benchmark
@@ -34,4 +34,4 @@ g++ -o redis_benchmark src/logging.o src/xredis_admin.o src/redis_benchmark.o -l
 
 #Finally, we look to build the main benchmark
 g++ -c -o main_benchmark.o main_benchmark.cpp -std=c++11
-g++ -o src/Obj3.pb.cc src/logging.o src/configuration_manager.o src/event_dispatcher.o src/globals.o src/zmqo.o src/obj3.o src/couchbase_admin.o src/xredis_admin.o src/document_manager.o main_benchmark.o -lpthread -lxredis -lzmq -lcouchbase -llog4cpp -luuid `pkg-config --cflags --libs protobuf hiredis` -std=c++11
+g++ -o main_benchmark src/Obj3.pb.cc src/logging.o src/configuration_manager.o src/event_dispatcher.o src/globals.o src/zmqo.o src/obj3.o src/couchbase_admin.o src/xredis_admin.o src/document_manager.o main_benchmark.o -lpthread -lxredis -lzmq -lcouchbase -llog4cpp -luuid `pkg-config --cflags --libs protobuf hiredis` -std=c++11
