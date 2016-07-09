6b81a359a91314595c6a3b6d1e6ce3b86fd4d194
ao-streetart <aostreetart9@gmail.com>
2016-07-08 21:12:12 -0400

    HTTP Properties added to test

--- src/http_test.cpp
+++ src/http_test.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <assert.h>
 
+//A String to store response data
 std::string data;
 
 //This is the callback that gets called when we recieve the response to the
@@ -26,6 +27,54 @@ int main(int argc, char* argv[])
 {
   if (argc > 0)
   {
+
+    //Variables to store URL's
+    std::string POSTURL = "";
+    std::string PUTURL = "";
+    std::string GETTURL = "";
+    std::string DELETEURL = "";
+
+    //Read the configuration file with URL info
+    std::string line;
+    std::ifstream file (argv[0]);
+
+    if (file.is_open()) {
+      while (getline (file, line) ) {
+        //Read a line from the property file
+
+        //Figure out if we have a blank or comment line
+        bool keep_going = true;
+        if (line.length() > 0) {
+          if (line[0] == '/' && line[1] == '/') {
+            keep_going=false;
+          }
+        }
+        else {
+          keep_going=false;
+        }
+
+        if (keep_going==true) {
+          int eq_pos = line.find("=", 0);
+          std::string var_name = line.substr(0, eq_pos);
+          std::string var_value = line.substr(eq_pos+1, line.length() - eq_pos);
+          if (var_name=="Put_URL") {
+            PUTURL=var_value;
+          }
+          if (var_name=="Post_URL") {
+            POSTURL=var_value;
+          }
+          else if (var_name=="Delete_URL") {
+            DELETEURL=var_value;
+          }
+          else if (var_name=="Get_URL") {
+            GETTURL=var_value;
+          }
+        }
+      }
+    }
+
+
+
     HttpAdmin ha;
 
     //-------------------------------GET--------------------------------------//
@@ -36,7 +85,7 @@ int main(int argc, char* argv[])
     curl_easy_setopt(ha.get_instance(), CURLOPT_WRITEFUNCTION, &writeCallback);
 
     //Send the request
-    bool success = ha.get(argv[0], 5);
+    bool success = ha.get(GETURL, 5);
     if (!success)
     {
       //We now have the full response
@@ -49,7 +98,7 @@ int main(int argc, char* argv[])
 
     //-------------------------------PUT--------------------------------------//
 
-    success = ha.put(argv[0], 5);
+    success = ha.put(PUTURL, 5);
     if (!success)
     {
       //We now have the full response
@@ -58,7 +107,7 @@ int main(int argc, char* argv[])
 
     //-------------------------------POST-------------------------------------//
 
-    success = ha.post(argv[0], "CLYMAN", 5);
+    success = ha.post(POSTURL, "CLYMAN", 5);
     if (!success)
     {
       //We now have the full response
@@ -67,7 +116,7 @@ int main(int argc, char* argv[])
 
     //------------------------------DELETE------------------------------------//
 
-    success = ha.del(argv[0], 5);
+    success = ha.del(DELETEURL, 5);
     if (!success)
     {
       //We now have the full response
