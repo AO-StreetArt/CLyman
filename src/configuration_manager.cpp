b58a95097f7fed78189669c5b355a3fe1490c0f9
ao-streetart <aostreetart9@gmail.com>
2016-08-28 22:13:19 -0400

    Config manager updates

--- src/configuration_manager.cpp
+++ src/configuration_manager.cpp
@@ -138,53 +138,6 @@ bool ConfigurationManager::configure_from_file (std::string file_path)
 
 //---------------------------Configure from Consul----------------------------//
 
-std::string ConfigurationManager::base64_decode(std::string const& encoded_string) {
-
-  static const std::string base64_chars =
-               "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-               "abcdefghijklmnopqrstuvwxyz"
-               "0123456789+/";
-
-  int in_len = encoded_string.size();
-  int i = 0;
-  int j = 0;
-  int in_ = 0;
-  unsigned char char_array_4[4], char_array_3[3];
-  std::string ret;
-
-  while (in_len-- && ( encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
-    char_array_4[i++] = encoded_string[in_]; in_++;
-    if (i ==4) {
-      for (i = 0; i <4; i++)
-        char_array_4[i] = base64_chars.find(char_array_4[i]);
-
-      char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-      char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-      char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
-
-      for (i = 0; (i < 3); i++)
-        ret += char_array_3[i];
-      i = 0;
-    }
-  }
-
-  if (i) {
-    for (j = i; j <4; j++)
-      char_array_4[j] = 0;
-
-    for (j = 0; j <4; j++)
-      char_array_4[j] = base64_chars.find(char_array_4[j]);
-
-    char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-    char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-    char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
-
-    for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
-  }
-
-  return ret;
-}
-
 std::vector<std::string> ConfigurationManager::split(std::string inp_string, char delim)
 {
 std::vector<std::string> elems;
@@ -235,7 +188,7 @@ std::string ConfigurationManager::get_consul_config_value(std::string key)
   }
 
   //Transform the object from base64
-  return base64_decode(resp_str);
+  return ca->base64_decode(resp_str);
 }
 
 //Configure based on the Services List and Key/Value store from Consul
