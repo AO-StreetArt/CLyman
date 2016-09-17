31acaa9a134263f649aaf9d38877a2df0f7fe980
ao-streetart <aostreetart9@gmail.com>
2016-09-17 11:33:52 -0400

    Test Fixes

--- src/test/configuration_test.cpp
+++ src/test/configuration_test.cpp
@@ -56,21 +56,25 @@ int main( int argc, char** argv )
   std::vector<RedisConnChain> RedisConnectionList = cm.get_redisconnlist();
   int conn_list_size = RedisConnectionList.size();
 
-  RedisConnChain redis_chain = RedisConnectionList[0];
-  assert( redis_chain.ip == "127.0.0.1" );
-  assert( redis_chain.port == 6379 );
-  assert( redis_chain.password == "test" );
-  assert( redis_chain.pool_size == 2);
-  assert( redis_chain.timeout == 5);
-  assert( redis_chain.role == 0);
+  if (conn_list_size > 0) {
+    RedisConnChain redis_chain = RedisConnectionList[0];
+    assert( redis_chain.ip == "127.0.0.1" );
+    assert( redis_chain.port == 6379 );
+    assert( redis_chain.password == "test" );
+    assert( redis_chain.pool_size == 2);
+    assert( redis_chain.timeout == 5);
+    assert( redis_chain.role == 0);
+  }
 
-  RedisConnChain redis_chain2 = RedisConnectionList[1];
-  assert( redis_chain2.ip == "127.0.0.1" );
-  assert( redis_chain2.port == 6380 );
-  assert( redis_chain2.password == "test2" );
-  assert( redis_chain2.pool_size == 2);
-  assert( redis_chain2.timeout == 5);
-  assert( redis_chain2.role == 0);
+  if (conn_list_size > 1) {
+    RedisConnChain redis_chain2 = RedisConnectionList[1];
+    assert( redis_chain2.ip == "127.0.0.1" );
+    assert( redis_chain2.port == 6380 );
+    assert( redis_chain2.password == "test2" );
+    assert( redis_chain2.pool_size == 2);
+    assert( redis_chain2.timeout == 5);
+    assert( redis_chain2.role == 0);
+  }
 
   shutdown_logging_submodules();
   delete cli;
