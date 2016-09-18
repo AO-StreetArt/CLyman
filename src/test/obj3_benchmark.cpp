a67df19615b4ccbbbb270be5d0e6d8b0cdeaa436
ao-streetart <aostreetart9@gmail.com>
2016-09-18 01:05:17 -0400

    Object Level Locking Fixes

--- src/test/obj3_benchmark.cpp
+++ src/test/obj3_benchmark.cpp
@@ -55,28 +55,28 @@ std::string owner = obj1->get_owner();
 BENCHMARK(Transform, Translate, 5, 50)
 {
 
-obj1->translate(1.0, 1.0, 1.0);
+obj1->translate(1.0, 1.0, 1.0, true);
 
 }
 
 BENCHMARK(Transform, RotateEuler, 5, 50)
 {
 
-obj1->rotate(45.0, 45.0, 45.0);
+obj1->rotate(45.0, 45.0, 45.0, true);
 
 }
 
 BENCHMARK(Transform, RotateQuaternion, 5, 50)
 {
 
-obj1->rotate( (sqrt (3.0) / 3.0), (sqrt (3.0) / 3.0), (sqrt (3.0) / 3.0), 45.0);
+obj1->rotate( (sqrt (3.0) / 3.0), (sqrt (3.0) / 3.0), (sqrt (3.0) / 3.0), 45.0, true);
 
 }
 
 BENCHMARK(Transform, Scale, 5, 50)
 {
 
-obj1->resize(2.0, 2.0, 2.0);
+obj1->resize(2.0, 2.0, 2.0, true);
 
 }
 
