ba4908956b531787c82321ad5eed45474678367b
Alex Barry <aostreetart9@gmail.com>
2017-06-03 23:45:16 -0400

    Introduction of parallel travis builds for success vs failure tests

--- scripts/linux/run_tests.sh
+++ scripts/linux/run_tests.sh
@@ -4,9 +4,9 @@ TEST_SUITE=$1
 HOME_DIR=$2
 
 if [ $TEST_SUITE != "failure" ]; then
-  cd $HOME_DIR && ./scripts/linux/run_failure_tests.sh
+  cd $HOME_DIR/scripts/linux/ && ./run_failure_tests.sh
 fi
 
 if [ $TEST_SUITE != "success" ]; then
-  cd $HOME_DIR && ./scripts/linux/run_success_tests.sh
+  cd $HOME_DIR/scripts/linux/ && ./run_success_tests.sh
 fi
